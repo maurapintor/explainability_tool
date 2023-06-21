9fe9f7be0d5592184b6ec809f5582fb5101ca11b
Angelo Sotgiu <angelo.sotgiu@unica.it>
2023-06-21 11:41:52 +0200

    add favicon in explainer html

--- commit_classifier/explainer/commit_explainer.py
+++ commit_classifier/explainer/commit_explainer.py
@@ -13,6 +13,8 @@ header = ["<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\"",
           "<head>",
           "<title>Explainability Tool</title>",
           "<link rel=\"stylesheet\" href=\"static/styles.css\" type=\"text/css\">",
+          "<link id=\"favicon\" rel=\"icon\" type=\"image/x-icon\" href=\"assets/images/favicon.ico\">",
+          "<link rel=\"apple-touch-icon\" type=\"image/x-icon\" href=\"assets/images/favicon.ico\">",
           "<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js\" "
           "integrity=\"sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz\" "
           "crossorigin=\"anonymous\"></script>",
